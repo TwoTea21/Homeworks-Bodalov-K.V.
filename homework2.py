a45a64df2b042ccead16bba99c481e154f158599
TwoTea21 <bodalovkv@gmail.com>
2024-05-08 22:31:37 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+name = 'Konstantin'
+print ('Name: ' + name)
+age = 18
+print ('Age: ' + str(age))
+age = age + 1
+print ('New Age: ' + str(age))
+is_student = True
+print ('Is Student: ' + str(is_student))

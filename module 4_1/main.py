80a4e08ba46a62c922076d52f72652ebb3331992
TwoTea21 <bodalovkv@gmail.com>
2024-06-06 14:26:35 +0300

    Add files via upload

---
+++ module 4_1/main.py
@@ -0,0 +1,7 @@
+from truth_math import divide as tr_div
+from fake_math import divide as fk_div
+
+result1 = fk_div(60, 5)
+result2 = fk_div(4, 0)
+result3 = tr_div(51, 6)
+result4 = tr_div(16, 0)
---
+++ module 4_1/truth_math.py
@@ -0,0 +1,8 @@
+def divide(first, second):
+    if second != 0:
+        result = first / second
+        print('Ответ равен:', result)
+
+        print('На ноль делить нельзя :(')
+        return
+
